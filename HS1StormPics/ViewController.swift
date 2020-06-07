9a72f322b2b369dde02d1306051b9eeccf1b9244
Aaron Shackelford <burntcreamlatte@icloud.com>
2020-06-06 19:42:36 -0700

    Initial Commit

---
+++ HS1StormPics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HS1StormPics
+//
+//  Created by Aaron Shackelford on 6/6/20.
+//  Copyright © 2020 Aaron Shackelford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
